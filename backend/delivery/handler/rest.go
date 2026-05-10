facc8d3ef8f2d3ba9ae48d630b8f04dfa1fb3d6f
Lidwina <lidwinaeleonora@gmail.com>
2026-05-11 06:39:51 +0700

    added cors for customer/client app

--- backend/delivery/handler/rest.go
+++ backend/delivery/handler/rest.go
@@ -4,10 +4,19 @@ import (
 	"qris-latency-optimizer/usecase/customer"
 	"qris-latency-optimizer/usecase/service"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Rest(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"*"}, 
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+    }))
+	
 	r.GET("/api/qris", service.GenerateDynamic)
 
 	r.GET("/ping", service.Ping)
