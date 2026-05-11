4747ad0ec4e6464d27135bf6b7dc7e6c81d87fb4
Lidwina <lidwinaeleonora@gmail.com>
2026-05-11 09:03:59 +0700

    fix IP at customer/client app

--- customer-app/src/services/api.js
+++ customer-app/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const currentHostname = window.location.hostname;
+const API_BASE_URL = `http://${currentHostname}:8080/api`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- customer-app/vite.config.js
+++ customer-app/vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
     server: {
     host: '0.0.0.0',
+    host: true,
     port: 5174,
     strictPort: true,
   },
